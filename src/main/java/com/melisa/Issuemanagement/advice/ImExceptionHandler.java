a912ffc604eee2f8430c0f38e197a8041f3316b8
melis <melisagokmenn@hotmail.com>
2021-05-18 19:58:13 +0300

    Logs has been added.

--- src/main/java/com/melisa/Issuemanagement/advice/ImExceptionHandler.java
+++ src/main/java/com/melisa/Issuemanagement/advice/ImExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.melisa.Issuemanagement.advice;
 
+import com.melisa.Issuemanagement.api.ProjectController;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,8 +17,10 @@ import java.util.Date;
 @ControllerAdvice
 @RestController
 public class ImExceptionHandler extends ResponseEntityExceptionHandler {
+    private static Logger LOGGER = LoggerFactory.getLogger(ProjectController.class);
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<?> handleExceptions(Exception ex, WebRequest request){
+        LOGGER.error("ControllerAdvice -> ExceptionHandler -> ",ex ,request);
         ExceptionResponse exceptionResponse=new ExceptionResponse(new Date(),ex.getMessage());
         return new ResponseEntity<>(exceptionResponse, HttpStatus.EXPECTATION_FAILED);
     }
--- src/main/java/com/melisa/Issuemanagement/api/ProjectController.java
+++ src/main/java/com/melisa/Issuemanagement/api/ProjectController.java
@@ -5,15 +5,18 @@ import com.melisa.Issuemanagement.service.impl.ProjectServiceImpl;
 import com.melisa.Issuemanagement.util.ApiPaths;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
 
 @RestController
 @RequestMapping(ApiPaths.ProjectCtrl.CTRL)
 @Api(value = ApiPaths.ProjectCtrl.CTRL,description = "Project APIs")
 public class ProjectController {
     private final ProjectServiceImpl projectServiceImpl;
-
+    private static Logger LOGGER = LoggerFactory.getLogger(ProjectController.class);
     public ProjectController(ProjectServiceImpl projectServiceImpl) {
         this.projectServiceImpl = projectServiceImpl;
     }
@@ -21,6 +24,8 @@ public class ProjectController {
     @GetMapping("/{id}")
     @ApiOperation(value = "Get By Id Operation",response = ProjectDto.class)
     public ResponseEntity<ProjectDto> getById(@PathVariable(value = "id", required = true) Long id) {
+        LOGGER.info("ProjectController -> GetById " );
+        LOGGER.debug("ProjectController -> GetById -> PARAM: "+id );
         ProjectDto projectDto = projectServiceImpl.getById(id);
         return ResponseEntity.ok(projectDto);
     }
